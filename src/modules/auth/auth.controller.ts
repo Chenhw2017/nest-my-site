7d28c5cde38a50fb14969ca0dc1bac52eb4c8e42
陈红伟 <chenhw2019@gmail.com>
2023-09-12 16:07:04 +0530

    fix: test

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -2,11 +2,11 @@
  * @Author: 陈红伟 chenhw2019@gmail.com
  * @Date: 2023-06-30 06:42:59
  * @LastEditors: 陈红伟 chenhw2019@gmail.com
- * @LastEditTime: 2023-09-12 08:23:14
+ * @LastEditTime: 2023-09-12 16:04:29
  * @FilePath: /nest-my-site/src/app.controller.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Headers, Request } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
 @Controller('wx')
@@ -14,7 +14,7 @@ export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('callback')
-  getHello(@Body() wxCallBackPayload: any): string {
-    return this.authService.saveTicket(wxCallBackPayload);
+  getHello(@Body() wxCallBackPayload: any, @Request() req): string {
+    return this.authService.saveTicket(wxCallBackPayload, req.url);
   }
 }
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -2,7 +2,7 @@
  * @Author: 陈红伟 chenhw2019@gmail.com
  * @Date: 2023-09-11 12:38:28
  * @LastEditors: 陈红伟 chenhw2019@gmail.com
- * @LastEditTime: 2023-09-12 09:01:49
+ * @LastEditTime: 2023-09-12 16:06:49
  * @FilePath: /nest-my-site/src/modules/auth/auth.service.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -15,9 +15,16 @@ import { RedisService } from '../../shared/services/redis.service';
 export class AuthService {
   constructor(private redisService: RedisService) {}
 
-  saveTicket(xmlParams: any) {
-    console.log(xmlParams.xml, 123, JSON.stringify(xmlParams.xml));
-    this.redisService.set('88', JSON.stringify(xmlParams.xml), 12 * 60);
+  getAuthLink(): string {
+    let ret: string;
+    return ret;
+  }
+  saveTicket(xmlParams: any, url: string) {
+    console.log(xmlParams, 123, JSON.stringify(xmlParams.xml));
+    const str =
+      '{"Encrypt":["\\n        LDFAmKFr7U/RMmwRbsR676wjym90byw7+hhh226e8bu6KVYy00HheIsVER4eMgz/VBtofSaeXXQBz6fVdkN2CzBUaTtjJeTCXEIDfTBNxpw/QRLGLq\\nqMZHA3I+JiBxrrSzd2yXuXst7TdkVgY4lZEHQcWk85x1niT79XLaWQog+OnBV31eZbXGPPv8dZciKqGo0meTYi+fkMEJdyS8OE7NjO79vpIyIw7hMBtEXPBK/tJGN5m5SoAS\\n6I4rRZ8Zl8umKxXqgr7N8ZOs6DB9tokpvSl9wT9T3E62rufaKP5EL1imJUd1pngxy09EP24O8Th4bCrdUcZpJio2l11vE6bWK2s5WrLuO0cKY2GP2unQ4fDxh0L4ePmNOVFJ\\nwp9Hyvd0BAsleXA4jWeOMw5nH3Vn49/Q/ZAQ2HN3dB0bMA+6KJYLvIzTz/Iz6vEjk8ZkK+AbhW5eldnyRDXP/OWfZH2P3WQZUwc/G/LGmS3ekqMwQThhS2Eg5t4yHv0mAIei\\n07Lknip8nnwgEeF4R9hOGutE9ETsGG4CP1LHTQ4fgYchOMfB3wANOjIt9xendbhHbu51Z4OKnA0F+MlgZomiqweT1v/+LUxcsFAZ1J+Vtt0FQXElDKg+YyQnRCiLl3I+GJ/c\\nxSj86XwClZC3NNhAkVU11SvxcXEYh9smckV/qRP2Acsvdls0UqZVWnPtzgx8hc8QBZaeH+JeiaPQD88frNvA== \\n    "],"MsgSignature":[""],"TimeStamp":["1411034505"],"Nonce":[""]}';
+    this.redisService.set('xml', JSON.stringify(xmlParams), 12 * 60 * 60);
+    this.redisService.set('url', url, 12 * 60 * 60);
     return 'success';
   }
 }
