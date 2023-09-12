08381f3dd5e6c848e4b71f3aadd8d21eeb481012
陈红伟 <chenhw2019@gmail.com>
2023-09-12 13:02:58 +0530

    fix: 调整端口

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@
  * @Author: 陈红伟 chenhw2019@gmail.com
  * @Date: 2023-06-30 06:42:59
  * @LastEditors: 陈红伟 chenhw2019@gmail.com
- * @LastEditTime: 2023-09-12 08:53:14
+ * @LastEditTime: 2023-09-12 13:02:24
  * @FilePath: /nest-my-site/src/main.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -18,6 +18,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(bodyParser.xml());
-  await app.listen(3001);
+  await app.listen(80);
 }
 bootstrap();
